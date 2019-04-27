889a842c87de587fcd0502ad7f7d3b9c59ef70c0
Unknown <david.montoro@behimba.com>
2019-04-27 13:47:33 +0200

    adding axios + http login for first call

--- src/sigfox-js.ts
+++ src/sigfox-js.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosInstance } from 'axios'
+
 import { ConfigParams } from './types/config-params'
+import { Device } from './types/device'
 import { API_CONFIG } from './config/constants'
 
 /**
@@ -7,12 +10,19 @@ import { API_CONFIG } from './config/constants'
  */
 
 export default class SigfoxApi {
+  public rest: AxiosInstance
+  public customParams: object
+
   /**
    * @constructor
    * @param {ConfigParams} customParams ID Client Setup
    */
   constructor(ConfigParams: ConfigParams) {
-    //
+    this.rest = axios.create({ baseURL: API_CONFIG.baseURL })
+    this.customParams = {
+      auth: ConfigParams,
+      API_CONFIG
+    }
   }
 
   /**
@@ -20,6 +30,9 @@ export default class SigfoxApi {
    * @returns {device}
    */
   public getDeviceInfo(deviceId: string) {
-    //
+    return this.rest
+      .get<Device>(API_CONFIG.baseURL + `/devices/${deviceId}`, this.customParams)
+      .then(response => response.data)
+      .catch(error => error.response.statusText)
   }
 }

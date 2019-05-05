9251f7d785cc5f85be8df6bd678182ac0d29920a
David ML <david.montoro@behimba.com>
2019-05-05 19:38:19 +0200

    feat(edit tools setup): edit tools setup
    edit tools setup

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,9 +1,9 @@
 const { cd, exec, echo, touch } = require('shelljs')
-const { readFileSync } = require('fs')
+import { readFileSync } from 'fs'
 const url = require('url')
 
 let repoUrl
-let pkg = JSON.parse(readFileSync('package.json') as any)
+const pkg = JSON.parse(readFileSync('package.json') as any)
 if (typeof pkg.repository === 'object') {
   if (!pkg.repository.hasOwnProperty('url')) {
     throw new Error('URL does not exist in repository section')

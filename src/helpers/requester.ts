21e9f181e4a39038285650e72bfe84b2824edb0e
septk <septk.developer@gmail.com>
2019-05-12 12:51:13 +0200

    feat(requester post function): requester post reusable function

--- src/helpers/requester.ts
+++ src/helpers/requester.ts
@@ -12,7 +12,14 @@ export class Requester {
 
   public async get<T>(info: any): Promise<T> {
     return this.rest
-      .get<any>(API_CONFIG.baseURL + info.url, this.config)
+      .get<T>(API_CONFIG.baseURL + info.url, this.config)
+      .then(response => response.data)
+      .catch(error => error.response.statusText)
+  }
+
+  public async post<T>(info: any): Promise<T> {
+    return this.rest
+      .post<T>(API_CONFIG.baseURL + info.url, info.data, this.config)
       .then(response => response.data)
       .catch(error => error.response.statusText)
   }

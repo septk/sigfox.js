cc15033e4fb722779e47d8e94488fd0ddd15d8c3
septk.developer@gmail.com <david.montoro@behimba.com>
2019-12-06 17:14:39 +0100

    chore: change lib output

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,7 +10,7 @@ const pkg = require('./package.json')
 const libraryName = 'sigfox-js'
 
 export default {
-  input: `src/index.ts`,
+  input: `src/${libraryName}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true }
